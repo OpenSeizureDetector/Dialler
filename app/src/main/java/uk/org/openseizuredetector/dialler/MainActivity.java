6e921a2ee08519c4c563e1c549ee8f839018ef19
Graham Jones <grahamjones139@gmail.com>
2020-08-20 20:54:35 +0100

    Working version that will dial the first number on the SMS contacts list.

--- app/src/main/java/uk/org/openseizuredetector/dialler/MainActivity.java
+++ app/src/main/java/uk/org/openseizuredetector/dialler/MainActivity.java
@@ -1,22 +1,83 @@
 package uk.org.openseizuredetector.dialler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "Dialler.MainActivity";
+    private boolean mPermissionsRequested = false;
+    private Button uninstallBtn;
+
+    private final String[] REQUIRED_PERMISSIONS = {
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.READ_CALL_LOG
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        OsdBroadcastReceiver br = new OsdBroadcastReceiver();
+        uninstallBtn = (Button)findViewById(R.id.uninstall_button);
+        uninstallBtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Log.v(TAG, "Uninstall button clicked");
+                        try {
+                            Intent intent = new Intent(Intent.ACTION_DELETE);
+                            intent.setData(Uri.parse("package:uk.org.openseizuredetector.dialler"));
+                            startActivity(intent);
+                        } catch (Exception ex) {
+                            Log.v(TAG, "exception starting uninstall activity " + ex.toString());
+                        }
+
+                    }
+                }
+        );
+
+        OsdBroadcastReceiver br = new OsdBroadcastReceiver(this);
         IntentFilter filter = new IntentFilter("uk.org.openseizuredetector.dialler.ALARM");
         this.registerReceiver(br, filter);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestPermissions();
+    }
+
+
+    public void requestPermissions() {
+        if (mPermissionsRequested) {
+            Log.i(TAG,"requestPermissions() - request already sent - not doing anything");
+        } else {
+            Log.i(TAG, "requestPermissions() - requesting permissions");
+            for (int i = 0; i < REQUIRED_PERMISSIONS.length; i++) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        REQUIRED_PERMISSIONS[i])) {
+                    Log.i(TAG, "shouldShowRationale for permission" + REQUIRED_PERMISSIONS[i]);
+                }
+            }
+            ActivityCompat.requestPermissions(this,
+                    REQUIRED_PERMISSIONS,
+                    42);
+            mPermissionsRequested = true;
+        }
+    }
+
+
 }
--- app/src/main/java/uk/org/openseizuredetector/dialler/OsdBroadcastReceiver.java
+++ app/src/main/java/uk/org/openseizuredetector/dialler/OsdBroadcastReceiver.java
@@ -3,13 +3,27 @@ package uk.org.openseizuredetector.dialler;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 import android.widget.Toast;
 
 public class OsdBroadcastReceiver extends BroadcastReceiver {
+    private final Context mContext;
     private String TAG = "OsdBroadcastReceiver";
+
+    public OsdBroadcastReceiver(Context context) {
+        mContext = context;
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        /*
+         Expects to receive an action uk.org.openseizuredetector.dialer.ALARM with extra data called NUMBERS which should be
+          an array of strings, which are the phone numbers to dial.
+          FIXME - at the moment this only dials the first number in the list, becasue this broadcast receiver exits
+                  when the dialler app is opened - we really need to start a new activity that calls the dialler app for each number
+                  in turn and checks if it is answered or not....and ideally plays a recorded message if successfully connected.
+         */
         Log.v(TAG,"onReceive()");
         if ("uk.org.openseizuredetector.dialler.ALARM".equals(intent.getAction())) {
             Log.v(TAG,"Received ALARM broadcast");
@@ -20,7 +34,12 @@ public class OsdBroadcastReceiver extends BroadcastReceiver {
             }
             Log.v(TAG,"Numbers="+receivedText);
 
-            context.getSystemService()
+            for (int i=0;i<NumbersArr.length;i++) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + NumbersArr[i]));
+                Log.v(TAG,"Calling "+NumbersArr[i]);
+                mContext.startActivity(callIntent);
+            }
+
         }
     }
 }
